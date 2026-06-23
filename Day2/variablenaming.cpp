24bb2691bb8ff405c624dc1a688fff1961d64f29
Seefat Sirin <seefatsirin48@gmail.com>
2026-06-23 13:30:50 +0530

    Updated code

--- Day2/variablenaming.cpp
+++ Day2/variablenaming.cpp
@@ -3,5 +3,12 @@ using namespace std;
 int main(){
     int alice=50;
     cout<<alice<<endl;
+    float radius;
+    radius=6;
+    cout<<radius<<endl;
+    int x1=10;
+    cout<<x1<<endl;
+    // int 1x =9;
+    // cout<<1x<<endl; // It gives error
 
 }
