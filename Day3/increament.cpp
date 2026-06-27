dc1e05b5c5e2c0566e2d64d147d1c2ae7e5a8900
Seefat Sirin <seefatsirin48@gmail.com>
2026-06-27 16:14:32 +0530

    Updated Project

---
+++ Day3/increament.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main (){
+    int x =10;
+    cout<<x<<endl;
+    x--;//x=x-1
+    cout<<x<<endl;
+    x++;//x=x+1
+    cout<<x<<endl;
+    --x;//x=x-1
+    cout<<x<<endl;
+    ++x;//x=x+1
+    cout<<x<<endl;
+}
