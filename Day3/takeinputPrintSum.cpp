b29ffd6cbe4d71e7c5f5d3f1b49b053bf0375aa6
Seefat Sirin <seefatsirin48@gmail.com>
2026-06-27 11:20:28 +0530

    Updated Project

---
+++ Day3/takeinputPrintSum.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main (){
+    int a,b,c;
+    cout<<"Enter 3 numbers";
+    cin>>a>>b>>c;
+    int sum=0;
+    sum=a+b+c;
+    cout<<sum<<endl;
+    
+}
