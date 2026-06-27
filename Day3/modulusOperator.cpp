3ed86c7754b0e2b1267c9a90e417b11eb043275c
Seefat Sirin <seefatsirin48@gmail.com>
2026-06-27 12:11:28 +0530

    Updated Pprojet

--- Day3/modulusOperator.cpp
+++ Day3/modulusOperator.cpp
@@ -3,5 +3,10 @@ using namespace std;
 int main(){
     cout<<51%22<<endl;
     cout<<25%5<<endl;
+    //cout<<25%0<<endl;//error divided by 0
+    //Properties of Modulus
+    cout<<45%241<<endl;//a%b=a if a<b
+    cout<<7%-2<<endl;//a%(-b)=a%b
+    cout<<-34%10<<endl; //-a%b =-(a%b)
 
 }
