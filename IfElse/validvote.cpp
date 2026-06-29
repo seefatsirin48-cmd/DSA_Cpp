cc03ad9ee15887f934de2637b56fd2c20ab65f51
Seefat Sirin <seefatsirin48@gmail.com>
2026-06-29 23:56:54 +0530

    Updated project

--- IfElse/validvote.cpp
+++ IfElse/validvote.cpp
@@ -1,5 +1,14 @@
 #include<iostream>
 using namespace std;
 int main (){
-    
+    int age;
+    cout<<"Enter age:";
+    cin>>age;
+    if(age>=18){
+        cout<<"Eligible for vote:";
+    }
+    else{
+        cout<<"Not Eligible for vote:";
+    }
+    cout<<"Demonstration of if else working";
 }
